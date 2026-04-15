4ff84edfc7fa156c4a5f75d18edb280a1ae735ab
Justocel <jcelsi@itba.edu.ar>
2026-04-15 10:47:42 -0300

    Load and display info for mercadolibre ventas dataset
    Added loading and info display for mercadolibre ventas data.

--- Datos2/Dataframes.py
+++ Datos2/Dataframes.py
@@ -6,8 +6,12 @@ datos_argenprop_alquileres = pd.read_csv('Datos2/datos_argenprop_alquiler.tsv', 
 datos_argenprop_ventas = pd.read_csv('Datos2/datos_argenprop_ventas (1).tsv', sep = '\t' )
 datos_remax_alquileres = pd.read_csv('Datos2/datos_remax_alquiler.csv')
 datos_remax_ventas = pd.read_csv('Datos2/datos_remax_venta.csv')
+datos_mercadolibre_ventas = pd.read_csv('Datos2/datos_mercadolibre_ventas.csv')
+
+#Se verifica la correcta carga de datos y que coinciden los tipos de variable con tipo de dato
 
 print(datos_argenprop_alquileres.info())
 print(datos_argenprop_ventas.info())
 print(datos_remax_alquileres.info())
 print(datos_remax_ventas.info())
+print(datos_mercadolibre_ventas.info())
